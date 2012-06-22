1f02c647ec7fe70830ec76d69c6557fb86d88a6c
Vlad Stremousov <vladst@post.jce.ac.il>
2012-06-22 10:16:09 -0700

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GradesJCE2012::Application.config.secret_token = '272bd81655453c8484ca99a7cb8edfbcec4a2064bdd8ead8ff5d39d007a483a951b6badca82a7b397f65c37c9ebfb8dc26d52d99fba0b9d06025cd333a090eef'

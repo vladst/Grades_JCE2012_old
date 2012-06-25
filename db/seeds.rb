08d525c18b1b611dac82c3a264d7489e45136ee1
Vlad <vladst@post.jce.ac.il>
2012-06-25 15:29:32 -0700

    permissions

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Gclass.create(:class_id => '6a')
+Gclass.create(:class_id => '9b')
+Gclass.create(:class_id => '7c')
+Gclass.create(:class_id => '10a')
+
+Subject.create(:subject_id => 'History')
+Subject.create(:subject_id => 'Math')
+Subject.create(:subject_id => 'Soft eng')
+Subject.create(:subject_id => 'Geo')
+Subject.create(:subject_id => 'Calculus')
